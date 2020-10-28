957176242086e1871170cf932360cac62bff3844
gitx5622 <ggitau5622@gmail.com>
2020-10-28 11:38:12 +0300

    changed port

--- src/constants.js
+++ src/constants.js
@@ -2,9 +2,9 @@
 let API_ROUTE;
 
 if (process.env.NODE_ENV === 'production') {
-    API_ROUTE = 'https://admin.likee.co.ke'
+    API_ROUTE = 'https://bodaorda.jobhub.co.ke'
 }else {
-    API_ROUTE = 'http://127.0.0.1:9000';
+    API_ROUTE = 'http://127.0.0.1:3000';
 }
 
 export default API_ROUTE;
